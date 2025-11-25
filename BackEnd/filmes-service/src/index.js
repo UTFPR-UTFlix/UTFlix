bf1d19394a435150e2855fdbad01ab41b4430c3c
Gabriel Brandeli <gabrielbrandeli@gmail.com>
2025-11-25 16:08:17 -0300

    fase 2 concluida

--- BackEnd/filmes-service/src/index.js
+++ BackEnd/filmes-service/src/index.js
@@ -1,22 +1,23 @@
-import express from "express";
-import routerFilmes from "./routes/filmeRoutes.js";
-import generoRouter from './routes/generoRouter.js';
-import atorRouter from './routes/atorRoutes.js'; // 1. IMPORTAR
-import cors from "cors";
-import swaggerUi from 'swagger-ui-express';
-import swaggerSpec from './docs/swagger.js';
+// src/index.js
+const express = require('express');
+const routerFilmes = require('./routes/filmeRoutes.js');
+const generoRouter = require('./routes/generoRouter.js');
+const atorRouter = require('./routes/atorRoutes.js');
+const cors = require('cors');
+const swaggerUi = require('swagger-ui-express');
+const swaggerSpec = require('./docs/swagger.js');
 
 const app = express();
 app.use(express.json());
-app.use(cors({ origin: "http://localhost:8080" }));
+app.use(cors({ origin: 'http://localhost:8080' }));
 app.use('/swagger', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+app.use('/filmes', routerFilmes);
+app.use('/generos', generoRouter);
+app.use('/atores', atorRouter);
 
-app.use("/filmes", routerFilmes);
-app.use("/generos", generoRouter);
-app.use("/atores", atorRouter); // 2. USAR
-
-const PORT = process.env.PORT || 3001;
 if (process.env.NODE_ENV !== 'test') {
+  const PORT = process.env.PORT || 3001;
   app.listen(PORT, () => console.log(`🎬 Filme service rodando na porta ${PORT}`));
 }
-export default app;
+
+module.exports = app;
